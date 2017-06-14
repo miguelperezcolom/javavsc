68b784d151de1ee327c310eff694cb5c3c4eb7bc
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2017-06-14 21:11:13 +0200

    added first test java version

--- c/main.cpp
+++ c/main.cpp
@@ -2,37 +2,103 @@
 #include <chrono>
 
 
-void test(int i);
+void test1(int i);
+int test2(int i);
+long test3(int pos);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 
 
-    for (int i = 0; i < 1000; i++) {
-        test(i);
-    }
+	for (int i = 0; i < 100; i++) {
+		std::cout << test3(i) << "\n";
+	}
 
 
 
-    return 0;
+	return 0;
 }
 
+void test1(int pos) {
+	auto start = std::chrono::high_resolution_clock::now();
 
-void test(int pos) {
-    auto start = std::chrono::high_resolution_clock::now();
-
-    // operation to be timed ...
-
-    for (int i = 0; i < 10000; i++) {
-        i++;
-    }
+	// operation to be timed ...
+	int total = 0;
+	for (int i = 0; i < 10000; i++) {
+		total += i;
+	}
 
 
-    auto finish = std::chrono::high_resolution_clock::now();
+	auto finish = std::chrono::high_resolution_clock::now();
 
-    std::cout << "iteration "
-              << pos << " took " <<
-              std::chrono::duration_cast<std::chrono::nanoseconds>(finish-start).count()
-              << "ns\n";
+
+	std::cout << "iteration "
+			  << pos << " took " <<
+			  std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count()
+			  << "ns\n";
+
+}
+
+int test2(int pos) {
+	auto start = std::chrono::high_resolution_clock::now();
+
+	// operation to be timed ...
+	int total = 0;
+	for (int i = 0; i < 10000; i++) {
+		total += i - total;
+	}
+
+
+	auto finish = std::chrono::high_resolution_clock::now();
+
+
+	std::cout << "iteration "
+		<< pos << " took " <<
+		std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count()
+		<< "ns\n";
+
+	return total;
+}
+
+long test3(int pos) {
+	int NDIGITS = 100;          //max digits to compute
+	int LEN = (NDIGITS/4+1)*14;   //nec. array length
+
+	long a[LEN];                   //array of 4-digit-decimals
+	long b;                        //nominator prev. base
+	long c = LEN;                  //index
+	long d;                        //accumulator and carry
+	long e = 0;                    //save previous 4 digits
+	long f = 10000;                //new base, 4 decimal digits
+	long g;                        //denom previous base
+	long h = 0;                    //init switch
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for(; (b=c-=14) > 0 ;){    //outer loop: 4 digits per loop
+		for(; --b > 0 ;){      //inner loop: radix conv
+			d *= b;            //acc *= nom prev base
+			if( h == 0 )
+				d += 2000*f;   //first outer loop
+			else
+				d += a[b]*f;   //non-first outer loop
+			g=b+b-1;           //denom prev base
+			a[b] = d % g;
+			d /= g;            //save carry
+		}
+		h = printf("%d",e+d/f);//print prev 4 digits
+		d = e = d % f;         //save currently 4 digits
+		//assure a small enough d
+	}
+
+	auto finish = std::chrono::high_resolution_clock::now();
+
+
+	std::cout << "iteration "
+			  << pos << " took " <<
+			  std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count()
+			  << "ns\n";
+
+	return h;
 
 }
--- java/src/main/java/io/mateu/javavsc/AddIntegers.java
+++ java/src/main/java/io/mateu/javavsc/AddIntegers.java
@@ -12,21 +12,80 @@ public class AddIntegers
         System.out.println( "Hello World!" );
 
         for (int i = 0; i < 1000; i++) {
-            test(i);
+            System.out.println(test3(i));
         }
 
     }
 
-    public static void test(int pos) {
+    public static void test1(int pos) {
         long t0 = System.nanoTime();
 
+        int total = 0;
         for (int i = 0; i < 10000; i++) {
-            i++;
+            total += i;
         }
 
         long t1 = System.nanoTime();
 
         System.out.println("iteration " + pos
                 + " took " + (t1 - t0) + " ns.");
+
+    }
+
+    public static int test2(int pos) {
+        long t0 = System.nanoTime();
+
+        int total = 0;
+        for (int i = 0; i < 10000; i++) {
+            total += i - total;
+        }
+
+        long t1 = System.nanoTime();
+
+        System.out.println("iteration " + pos
+                + " took " + (t1 - t0) + " ns.");
+
+        return total;
+    }
+
+    public static double test3(int pos) {
+
+        int NDIGITS = 100;          //max digits to compute
+        int LEN = (NDIGITS/4+1)*14;
+
+        long[] a = new long[LEN];                   //array of 4-digit-decimals
+        int b = 0;                        //nominator prev. base
+        int c = LEN;                  //index
+        long d = 0;                        //accumulator and carry
+        long e = 0;                    //save previous 4 digits
+        long f = 10000;                //new base, 4 decimal digits
+        long g = 0;                        //denom previous base
+        StringBuffer h = new StringBuffer();                    //init switch
+
+        long t0 = System.nanoTime();
+
+        for(; (b=c-=14) > 0 ;){    //outer loop: 4 digits per loop
+            for(; --b > 0 ;){      //inner loop: radix conv
+                d *= b;            //acc *= nom prev base
+                if( h.length() == 0 )
+                    d += 2000*f;   //first outer loop
+                else
+                    d += a[b]*f;   //non-first outer loop
+                g=b+b-1;           //denom prev base
+                a[b] = d % g;
+                d /= g;            //save carry
+            }
+            h.append(e+d/f);//print prev 4 digits
+            d = e = d % f;         //save currently 4 digits
+            //assure a small enough d
+        }
+
+        long t1 = System.nanoTime();
+
+        System.out.println("iteration " + pos
+                + " took " + (t1 - t0) + " ns.");
+
+
+        return new Double(h.toString());
     }
 }
